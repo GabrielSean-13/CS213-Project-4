f42209aae3d390ab424199200b7931bed3a811ee
MarkHolleran <97909762+MarkHolleran@users.noreply.github.com>
2022-04-05 15:37:35 -0400

    added quantity to menuitem added subtotal and tax for orderingbasket updated ordering basket controller

--- src/main/java/rucafe/cs213project4/MenuItem.java
+++ src/main/java/rucafe/cs213project4/MenuItem.java
@@ -3,6 +3,7 @@ package rucafe.cs213project4;
 public class MenuItem {
 
     protected double price;
+    protected int quantity;
     //super class of all menu items (donut and coffee)
     //(any class defined for a menu item must extend this class)
 
@@ -10,6 +11,12 @@ public class MenuItem {
         //all subclasses override this method for calculate price of a menu item
 
         return this.price;
+
+    }
+
+    public int itemQuantity(){
+
+        return this.quantity;
 
     }
 
