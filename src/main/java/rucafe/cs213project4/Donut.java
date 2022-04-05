7a37bcf9b840aa95263af1d580fce3362519b4a7
Abhitej Bokka <abhitej.bokka@gmail.com>
2022-04-05 13:12:55 -0400

    donutMenu
    donutMenu

--- src/main/java/rucafe/cs213project4/Donut.java
+++ src/main/java/rucafe/cs213project4/Donut.java
@@ -1,5 +1,38 @@
-package rucafe.cs213project4;
+    package rucafe.cs213project4;
 
-public class Donut extends MenuItem {
-    //extends menuitem class
-}
+    public class Donut extends MenuItem {
+
+        protected String flavor;
+        protected String type;
+
+        public static final String YEAST = "Yeast Donut";
+        public static final String CAKE = "Cake Donut";
+        public static final String HOLE = "Donut Hole";
+
+        public static final double YEAST_PRICE = 1.59;
+        public static final double CAKE_PRICE = 1.79;
+        public static final double HOLE_PRICE = 0.39;
+
+
+        public Donut(String donutType, String donutFlavor){
+            type = donutType;
+            flavor = donutFlavor;
+
+            this.itemPrice();
+            //or just itemPrice() idk which
+        }
+
+        @Override
+        public double itemPrice(){
+            if(type.equals(YEAST)){
+                price = YEAST_PRICE;
+            }else if(type.equals(CAKE)){
+                price = CAKE_PRICE;
+            }else if(type.equals(HOLE)){
+                price = HOLE_PRICE;
+            }
+
+            return this.price;
+        }
+
+    }
--- src/main/java/rucafe/cs213project4/MenuItem.java
+++ src/main/java/rucafe/cs213project4/MenuItem.java
@@ -4,10 +4,11 @@ public class MenuItem {
     //super class of all menu items (donut and coffee)
     //(any class defined for a menu item must extend this class)
 
+    protected double price;
+
     public double itemPrice(){
         //all subclasses override this method for calculate price of a menu item
-
-
+        return this.price;
     }
 
 
