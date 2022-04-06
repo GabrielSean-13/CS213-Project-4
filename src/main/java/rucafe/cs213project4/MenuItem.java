e58385f737d54accbc76a73b13f083ad1bb787ee
MarkHolleran <97909762+MarkHolleran@users.noreply.github.com>
2022-04-05 20:57:36 -0400

    program can now run

--- src/main/java/rucafe/cs213project4/MenuItem.java
+++ src/main/java/rucafe/cs213project4/MenuItem.java
@@ -5,7 +5,6 @@ public class MenuItem {
 
     protected int quantity;
     protected double price;
-    protected int quantity;
     //super class of all menu items (donut and coffee)
     //(any class defined for a menu item must extend this class)
 
@@ -14,18 +13,9 @@ public class MenuItem {
         return this.price;
     }
 
-    public double getQuantity(){
+    public int getQuantity(){
         //all subclasses override this method for calculate price of a menu item
         return this.quantity;
     }
-
-    public int itemQuantity(){
-
-        return this.quantity;
-
-    }
-
-
-
 
 }
--- src/main/java/rucafe/cs213project4/ShopDonutController.java
+++ src/main/java/rucafe/cs213project4/ShopDonutController.java
@@ -7,10 +7,19 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 
+import static java.lang.Integer.parseInt;
+
+
 public class ShopDonutController {
-    //all instance variables in controllers should be private
+
+    public static final int INDEX_OF_YEAST_DONUT = 0;
+
+
     @FXML
     private ToggleButton cakeDonut;
+
+    @FXML
+    private ToggleGroup donutFlavor;
 
     @FXML
     private ToggleButton donutHoles;
@@ -34,6 +43,53 @@ public class ShopDonutController {
 
     @FXML
     void addToOrder(ActionEvent event) {
+        //look at donuttype
+        //look at donutflavor
+        //look at quantity
+        //create and put into order arraylist
+
+        System.out.println(donutFlavor.getSelectedToggle().toString());
+
+
+        if (donutType.getSelectedToggle() != null && donutFlavor.getSelectedToggle() != null && !(quantityOfOrder.getText().equals(""))){
+
+            //if here everything is filled out
+            //god level java workaround
+
+            String selectedDonutTypeButtonToString = donutType.getSelectedToggle().toString();
+
+            String selectedDonutFlavorButtonToString = donutFlavor.getSelectedToggle().toString();
+
+            String selectedDonutType = selectedDonutTypeButtonToString.substring(selectedDonutTypeButtonToString.indexOf("'")+1, selectedDonutTypeButtonToString.lastIndexOf("'"));
+
+            String selectedDonutFlavor = selectedDonutFlavorButtonToString.substring(selectedDonutFlavorButtonToString.indexOf("'")+1, selectedDonutFlavorButtonToString.lastIndexOf("'"));
+
+
+
+
+            Donut newDonutOrder = new Donut(selectedDonutType, selectedDonutFlavor);
+
+            newDonutOrder.quantity = parseInt(quantityOfOrder.getText());
+
+            //Order.addOrder(newDonutOrder);
+
+            System.out.println(newDonutOrder.toString());
+
+
+
+
+
+
+
+
+
+        }else {
+            //make pop up that says to populate
+            System.out.println("error please populate all fields");
+
+
+
+        }
 
     }
 
